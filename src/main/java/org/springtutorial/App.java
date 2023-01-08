e9ea0f884501171727f641f3329018f6de52985d
pintu_yadav <pintukumaryadav.min18@itbhu.ac.in>
2023-01-08 00:13:26 -0800

    adding spring dependencies

--- src/main/java/org/springtutorial/App.java
+++ src/main/java/org/springtutorial/App.java
@@ -1,13 +1,18 @@
 package org.springtutorial;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springtutorial.core.Employ;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        Employ emp1= (Employ) context.getBean("employ1");
+        System.out.println(emp1);
+
+
     }
 }
